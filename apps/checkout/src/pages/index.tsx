002469b1e0ede186a049d454b7e43f38aaa18295
Nemanja <nemanja.d.krstic@gmail.com>
2023-01-24 12:22:29 +0100

    Add close icon

--- apps/checkout/src/pages/index.tsx
+++ apps/checkout/src/pages/index.tsx
@@ -1,5 +1,15 @@
+import { CloseIcon } from "@xola/ui-kit";
+
 const Index = () => {
-    return <main className="ml-auto h-screen max-h-screen max-w-2xl bg-white p-4">Checkout App</main>;
+    return (
+        <div className="relative ml-auto h-screen max-h-screen max-w-2xl bg-white p-4">
+            <button className="text-secondary absolute top-2 -left-10 h-8 w-8 rounded-full bg-white">
+                <CloseIcon />
+            </button>
+
+            <main className="max-h-full overflow-y-auto">Checkout App</main>
+        </div>
+    );
 };
 
 export default Index;
---
+++ apps/checkout/types.d.ts
@@ -0,0 +1,1 @@
+declare module "@xola/ui-kit";
